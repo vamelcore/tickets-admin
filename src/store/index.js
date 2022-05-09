2ab0fd9750181cbb1a0838a31c6ced987f96bddb
VITALIY <vamelcore@gmail.com>
2022-05-10 02:04:37 +0300

    Feat: Added Vuex persistence

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
 import { createStore } from 'vuex'
+import VuexPersistence from 'vuex-persist'
 import auth from './modules/auth'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 const store = createStore({
   state: {
@@ -21,7 +26,8 @@ const store = createStore({
   },
   modules: {
     auth
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
 
 export default store
