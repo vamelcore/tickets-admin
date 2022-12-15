efd8ac46fe61a9b04199ff65d44b7a37fa49c62e
VITALIY <vamelcore@gmail.com>
2022-12-15 23:35:12 +0200

    Feat: fix password restore component

--- src/views/auth/Restore.vue
+++ src/views/auth/Restore.vue
@@ -1,28 +1,55 @@
 <template>
-  <form class="start-page-form">
-    <div class="start-page-form__title">Восстановление пароля</div>
-    <div class="form__text">Для сброса пароля введите ваш E-mail</div>
+  <form class="start-page-form" @submit.prevent="restore">
+    <div class="start-page-form__title">{{ $t('auth.restore.title') }}</div>
+    <div class="form__text">{{ $t('auth.restore.forRestoreEnterEmail') }}</div>
     <div class="form__group">
-      <label class="form__label">E-mail</label>
-      <div class="form__full">
+      <label class="form__label">{{ $t('auth.restore.inputLabelEmail') }}</label>
+      <div class="form__full start-page-form__input" :class="[validations.email]">
         <input class="form__input" type="email" required="required" v-model="email"/>
+        <span class="checked--icon">
+          <img src="@/assets/svg/checked.svg">
+        </span>
       </div>
     </div>
-    <router-link class="btn" :to="{name:'restore'}">Сбросить пароль</router-link>
-    <router-link class="form__link" :to="{name:'login'}">Авторизация</router-link>
+    <p class="success" v-if="success">{{ $t('auth.restore.successMessage') }}</p>
+    <p class="error" v-if="validations.message">{{validations.message}}</p>
+    <button class="btn" :to="{name:'restore'}">{{ $t('auth.restore.buttonRestore') }}</button>
+    <router-link class="form__link" :to="{name:'login'}">{{ $t('auth.restore.buttonAuthorization') }}</router-link>
   </form>
 </template>
+
 <script>
+import validationHelpers from "@/helpers/validationHelpers"
 export default {
   name: 'RestoreView',
   data: function() {
     return {
       email: null,
+      success: false,
+      validations: {
+        message: '',
+        email: '',
+      },
     }
   },
+  methods: {
+    ...validationHelpers,
+    restore: function() {
+      this.$store.dispatch('auth/restore', {'email':this.email})
+      .then(() => {
+        this.success = true
+      })
+      .catch((errors) => {
+        this.validationSet(this.validations, errors)
+      })
+    }
+  },
+  watch: {
+    email: function() {
+      this.validationClean(this.validations, 'email')
+    }
+  }
 }
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
